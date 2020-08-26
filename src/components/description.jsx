0ded706b55b24308f004452fe11cfbc8191952e0
Mikhail Strizhenov <ramory.lorch@icloud.com>
2020-08-26 21:06:08 +0300

    must add hover

--- src/components/description.jsx
+++ src/components/description.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { cn } from "@bem-react/classname";
 import "./styles/description.css";
 
 function Description(props) {
   let food = props.info;
+  const description = cn("Description");
+  console.log(props);
+  let slogan = () => {
+    if (props.isHovered && props.isSelected) {
+      return <span style={{ color: "#e62e7a" }}>Котэ не одобряет?</span>;
+    } else {
+      return "Сказачное заморское яство";
+    }
+  };
   return (
-    <div className="Description">
-      <div className="Description-slogan">Сказочное заморское яство</div>
+    <div className={description({ ended: food.isEnded })}>
+      <div className="Description-slogan">{slogan()}</div>
       <div className="Description-brandName">Нямушка</div>
       <div className="Description-taste">{food.taste}</div>
       <p className="Description-numberOfServings">
--- src/components/packOfFeed.jsx
+++ src/components/packOfFeed.jsx
@@ -2,43 +2,111 @@ import React, { useState } from "react";
 import "./styles/packOfFeed.css";
 import { cn } from "@bem-react/classname";
 import Description from "./description";
-import CatAndBall from "./catAndBall";
 
 function PackOfFeed(props) {
   let food = props.info;
   const [isSelected, setSelected] = useState(false);
+  const [isHovered, setHover] = useState(false);
   const packOfFeed = cn("PackOfFeed");
+  const packOfFeedBall = cn("PackOfFeed-ball");
+  const packOfFeedCat = cn("PackOfFeed-cat");
 
   const toogleSelect = () => {
-    if (isSelected) {
+    if (isSelected && !food.isEnded) {
       setSelected(false);
-    } else {
+    } else if (!food.isEnded) {
       setSelected(true);
     }
   };
 
-  let caption = isSelected ? (
-    food.description
-  ) : (
-    <span>
-      Чего сидишь? Порадуй котэ,{" "}
-      <a onClick={toogleSelect} className="PackOfFeed-caption_link" href="#">
-        купи
-      </a>
-      <span className="PackOfFeed-caption_link_">.</span>
-    </span>
-  );
+  let caption = () => {
+    if (isSelected) {
+      return food.description;
+    } else if (food.isEnded) {
+      return (
+        <span
+          style={{ color: "#ffff66" }}
+        >{`Печалька, ${food.taste} закончился.`}</span>
+      );
+    } else {
+      return (
+        <span>
+          Чего сидишь? Порадуй котэ,{" "}
+          <a
+            onClick={toogleSelect}
+            className="PackOfFeed-caption_link"
+            href="#"
+          >
+            купи
+          </a>
+          <span className="PackOfFeed-caption_link_">.</span>
+        </span>
+      );
+    }
+  };
+
+  const deleteHoverColor = () => {
+    if (isSelected && !food.isEnded) {
+      setHover(false);
+    } else if (!food.isEnded) {
+      setHover(false);
+    }
+  };
+
+  const ref = React.createRef();
+
+  const setHoverColor = () => {
+    if (isSelected && !food.isEnded) {
+      setHover(true);
+    } else if (!food.isEnded) {
+      setHover(true);
+      console.log(
+        packOfFeedBall({
+          selected: isSelected,
+          ended: food.isEnded,
+          hover: isHovered,
+        })
+      );
+    }
+  };
 
   return (
     <figure>
       <div
+        onMouseOver={setHoverColor}
+        onMouseLeave={deleteHoverColor}
         onClick={toogleSelect}
-        className={packOfFeed({ selected: isSelected })}
+        ref={ref}
+        className={packOfFeed({
+          selected: isSelected,
+          ended: food.isEnded,
+          hover: isHovered,
+        })}
       >
-        <Description info={food} />
-        <CatAndBall info={food} isSelected={isSelected} />
+        <Description
+          info={food}
+          isHovered={isHovered}
+          isSelected={isSelected}
+        />
+        <img
+          className={packOfFeedCat({ ended: food.isEnded })}
+          src="/images/Cat.png"
+          alt="Тут должен быть котик"
+        />
+        <div
+          className={packOfFeedBall({
+            selected: isSelected,
+            ended: food.isEnded,
+            hover: isHovered,
+          })}
+        >
+          <div className="PackOfFeed-weight">
+            <span className="PackOfFeed-weight_value">{food.weight}</span>
+            <span className="PackOfFeed-weight_measure">кг</span>
+          </div>
+        </div>
       </div>
-      <figcaption className="PackOfFeed-caption">{caption}</figcaption>
+      <figcaption className="PackOfFeed-caption">{caption()}</figcaption>
     </figure>
   );
 }
